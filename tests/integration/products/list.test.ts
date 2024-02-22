73ebdc406f7e0080c019ffbad604e62449e2d971
fabricioFalcao <fabriciohfd@gmail.com>
2024-02-22 12:57:41 -0300

    Requisite 2 - Integration test for get products endpoint

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,27 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app'
+import ProductModel from '../../../src/database/models/product.model';
+import productMocks from '../../mocks/productMocks';
+import { Product } from '../../../src/types/Product';
 
 chai.use(chaiHttp);
 
-describe('GET /products', function () { 
+describe('GET /products', function () {
   beforeEach(function () { sinon.restore(); });
 
+  it('should return status 200 and the list of all products from database', async function () {
+    // Arrange
+    const mockedList: Product[] = productMocks.productListFromDb
+    const mockFindAllReturn = mockedList.map((product) => ProductModel.build(product))
+    sinon.stub(ProductModel, 'findAll').resolves(mockFindAllReturn)
+
+    // Act
+    const httpResponse = await chai.request(app).get('/products')
+
+    // Assert
+    expect(httpResponse.status).to.be.equal(200);
+    expect(httpResponse.body).to.be.deep.equal(mockedList)
+  })
 });
--- tests/mocks/productMocks.ts
+++ tests/mocks/productMocks.ts
@@ -14,7 +14,47 @@ const validProductFromDB: Product = {
   userId: 1
 }
 
+const productListFromDb: Product[] = [
+  {
+    id: 1,
+    name: "Excalibur",
+    price: "10 peças de ouro",
+    userId: 1,
+  },
+  {
+    id: 2,
+    name: "Espada Justiceira",
+    price: "20 peças de ouro",
+    userId: 1,
+  },
+  {
+    id: 3,
+    name: "Lira de Orfeu",
+    price: "1 peça de ouro",
+    userId: 2,
+  },
+  {
+    id: 4,
+    name: "Armadura de Aquiles",
+    price: "1 peça de ouro",
+    userId: 2,
+  },
+  {
+    id: 5,
+    name: "Harpa de Dagda",
+    price: "15 peças de ouro",
+    userId: 3,
+  },
+  {
+    id: 6,
+    name: "Martelo de Thor",
+    price: "30 peças de ouro",
+    userId: 1,
+  },
+];
+
 export default {
   validProductFromDB,
-  validProductRequest
+  validProductRequest,
+  productListFromDb
 }
